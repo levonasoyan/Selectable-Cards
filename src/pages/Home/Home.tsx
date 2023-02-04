2d4d3a4aea4c68dc467699cf2e6e3f5468561ae5
levonasoyan <levonasoyan1@gmail.com>
2023-02-04 19:35:46 +0400

    removed unnecessary variables

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,64 +1,44 @@
 import { useState } from "react";
 import "./Home.scss";
 const Home = () => {
-  const [selected, setSelected] = useState({ number: 0, active: false });
+  const [selected, setSelected] = useState(0);
   return (
     <div className="home_wrapper">
       <div className="flex_center">
         <div className="card">
-          <p>{selected.number}</p>
+          <p>{selected}</p>
         </div>
       </div>
       <div className="flex_wrap">
         <div
-          onClick={() => setSelected({ number: 1, active: !selected.active })}
-          className={
-            selected.number === 1
-              ? "card selected_card"
-              : "card"
-          }
+          onClick={() => setSelected(1)}
+          className={selected === 1 ? "card selected_card" : "card"}
         >
           <p>1</p>
         </div>
         <div
-          onClick={() => setSelected({ number: 2, active: !selected.active })}
-          className={
-            selected.number === 2
-              ? "card selected_card"
-              : "card"
-          }
+          onClick={() => setSelected(2)}
+          className={selected === 2 ? "card selected_card" : "card"}
         >
           <p>2</p>
         </div>
         <div
-          onClick={() => setSelected({ number: 3, active: !selected.active })}
-          className={
-            selected.number === 3
-              ? "card selected_card"
-              : "card"
-          }
+          onClick={() => setSelected(3)}
+          className={selected === 3 ? "card selected_card" : "card"}
         >
           <p>3</p>
         </div>
 
         <div
-          onClick={() => setSelected({ number: 4, active: !selected.active })}
-          className={
-             selected.number === 4
-              ? "card selected_card"
-              : "card"
-          }
+          onClick={() => setSelected(4)}
+          className={selected === 4 ? "card selected_card" : "card"}
         >
           <p>4</p>
         </div>
 
         <div
-          onClick={() => setSelected({ number: 5, active: !selected.active })}
-          className={
-             selected.number === 5
-              ? "card selected_card"
-              : "card"
-          }
+          onClick={() => setSelected(5)}
+          className={selected === 5 ? "card selected_card" : "card"}
         >
           <p>5</p>
         </div>
